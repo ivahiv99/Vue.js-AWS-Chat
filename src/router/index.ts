25bf2fbb28031d8c51cf97bd5d7f36766278a3f0
Vitaliy <ivahiv99@gmail.com>
2020-06-28 13:31:22 +0300

    work on routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,36 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import LogIn from '../views/auth/LogIn.vue';
+import SignUp from '../views/auth/SignUp.vue';
+
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: 'about' */ '../views/About.vue')
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: LogIn
+  },
+  {
+    path: '/sign-up',
+    name: 'Sign up',
+    component: SignUp
   }
 ];
 
---
+++ src/views/auth/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <form class="sign-up">
+      <div class="form-feild">
+        <label for=""></label>
+        <input type="text" class="form-input" />
+      </div>
+      <div class="form-feild">
+        <label for=""></label>
+        <input type="text" class="form-input" />
+      </div>
+      <div class="form-feild">
+        <label for=""></label>
+        <input type="text" class="form-input" />
+      </div>
+      <div class="form-feild">
+        <label for=""></label>
+        <input type="text" class="form-input" />
+      </div>
+      <!-- figure out router-link -->
+      <a href="#" class="">Already have an account?</a>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style scoped lang="scss"></style>

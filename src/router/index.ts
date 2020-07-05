d7600f2b74c995e4fa0ef2571cadaa4cc3ad624e
Vitaliy <ivahiv99@gmail.com>
2020-07-05 11:49:35 +0300

    routes & default route set up

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,6 @@ import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
 import LogIn from '../views/auth/LogIn.vue';
 import SignUp from '../views/auth/SignUp.vue';
-import MyProfile from '../views/MyProfile.vue';
 
 import Chats from '../views/Chats.vue';
 import ContactsList from '../views/ContactsList.vue';
@@ -14,26 +13,12 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    // redirect: '/chats',
-    // component: Home,
-    // children: [
-    //   {
-    //     path: 'chats',
-    //     name: 'Chats',
-    //     component: Chats
-    //   }
-    // ]
+    redirect: '/chats'
   },
-
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: 'about' */ '../views/About.vue')
+    path: '/chats',
+    name: 'Chats',
+    component: Chats
   },
   {
     path: '/contacts',
@@ -58,7 +43,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/my-profile',
     name: 'My Profile',
-    component: MyProfile
+    component: () => import('../views/MyProfile.vue')
   },
 ];
 
